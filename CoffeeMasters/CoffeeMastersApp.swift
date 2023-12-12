27d33567768060ec0dffd9d7b660476f1fb74b59
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2023-12-13 01:08:08 +0300

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Armağan Erdem Taş on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
