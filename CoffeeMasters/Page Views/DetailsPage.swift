308c501ee3b4faa261e7ed3ba7b0c8e0dc3ff6a3
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-01-24 22:39:01 +0300

    ProductItem, MenuItem and DetailsPage have been done.

--- CoffeeMasters/Page Views/DetailsPage.swift
+++ CoffeeMasters/Page Views/DetailsPage.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct DetailsPage: View {
+    
+    @State var quantity = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            Image("DummyImage")
+                .cornerRadius(5)
+                .frame(maxWidth: .infinity, idealHeight: 150, maxHeight: 150)
+                .padding(.top, 32)
+            Text("Product")
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                .padding(24)
+            HStack{
+                Text("$ 4.25 ea")
+                Stepper(value: $quantity, in: 1...10){}
+            }
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .padding(30)
+            
+            Text("Subtotal $4.25")
+                .bold()
+                .padding(12)
+            Button("Add \(quantity) to Cart"){
+                //Todo
+            }
+            .padding()
+            .frame(width: 250.0)
+            .background(Color("Alternative2"))
+            .foregroundColor(Color.black)
+            .cornerRadius(25)
+                
+        }
+        .navigationTitle("asd")
     }
 }
 
--- CoffeeMasters/Views/ProductItem.swift
+++ CoffeeMasters/Views/ProductItem.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct ProductItem: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("DummyImage")
+                .frame(width: 300, height: 150)
+                .background(Color("AccentColor"))
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Product Name")
+                        .font(.title3)
+                        .bold()
+                    Text("$ 4.25")
+                        .font(.caption)
+                }.padding(8)
+                Spacer()
+            }
+        }
+        .background(Color("SurfaceBackground"))
+        .cornerRadius(10)
+        .padding(.trailing)
     }
 }
 
